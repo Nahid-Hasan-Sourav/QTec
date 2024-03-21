073ddf3c14d975ff4418a330d23606376ae79e6a
Nahid Hasan Sourav <nahidhasansourav@gmail.com>
2024-03-21 22:02:57 +0600

    setup a template

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+
+
+    <title>@yield('title')</title>
+
+   @include('admin.includes.style')
+  </head>
+
+  <body>
+
+    <!-- ########## START: LEFT PANEL ########## -->
+   <!-- sl-sideleft -->
+    @include('admin.includes.leftsidebar')
+    <!-- ########## END: LEFT PANEL ########## -->
+
+    <!-- ########## START: HEAD PANEL ########## -->
+    <div class="sl-header">
+      <div class="sl-header-left">
+        <div class="navicon-left hidden-md-down"><a id="btnLeftMenu" href=""><i class="icon ion-navicon-round"></i></a></div>
+        <div class="navicon-left hidden-lg-up"><a id="btnLeftMenuMobile" href=""><i class="icon ion-navicon-round"></i></a></div>
+      </div><!-- sl-header-left -->
+      <div class="sl-header-right">
+        <nav class="nav">
+          <div class="dropdown">
+            <a href="" class="nav-link nav-link-profile" data-toggle="dropdown">
+              <span class="logged-name">Jane<span class="hidden-md-down"> Doe</span></span>
+              <img src="../img/img3.jpg" class="wd-32 rounded-circle" alt="">
+              <img src="{{asset('/')}}admin/assets/img/img9.jpg" alt="..." class="wd-32 rounded-circle">
+
+            </a>
+            <div class="dropdown-menu dropdown-menu-header wd-200">
+              <ul class="list-unstyled user-profile-nav">
+                <li><a href=""><i class="icon ion-ios-person-outline"></i> Edit Profile</a></li>
+                <li><a href=""><i class="icon ion-ios-gear-outline"></i> Settings</a></li>
+                <li><a href=""><i class="icon ion-ios-download-outline"></i> Downloads</a></li>
+                <li><a href=""><i class="icon ion-ios-star-outline"></i> Favorites</a></li>
+                <li><a href=""><i class="icon ion-ios-folder-outline"></i> Collections</a></li>
+                <li><a href=""><i class="icon ion-power"></i> Sign Out</a></li>
+              </ul>
+            </div><!-- dropdown-menu -->
+          </div><!-- dropdown -->
+        </nav>
+       <!-- navicon-right -->
+      </div><!-- sl-header-right -->
+    </div>
+
+
+
+    <!-- ########## START: MAIN PANEL ########## -->
+    <div class="sl-mainpanel">
+      <nav class="breadcrumb sl-breadcrumb">
+        <a class="breadcrumb-item" href="index.html">Starlight</a>
+        <span class="breadcrumb-item active">Dashboard</span>
+      </nav>
+
+      <div class="sl-pagebody">
+
+       <div>
+        @yield('body')
+       </div>
+
+      </div>
+    </div>
+
+    @include('admin.includes.scripts')
+  </body>
+</html>
